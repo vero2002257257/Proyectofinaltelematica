e1424eebc3e13b8e9612242e0ea61a697f2fa206
vero2002257257 <105020892+vero2002257257@users.noreply.github.com>
2022-05-12 17:23:07 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,26 @@
 #Este código lo puedes utilizar si deseas
 #1.Llamar las librerias
-from dash import Dash, html, dcc
-import plotly.graph objs as go
+from dash import Dash, html, dcc  #LIBRERIA SERVIDOR
 import pandas as pd
 
+import plotly.graph_objs as go
+
+#2.Base de datos
 app = Dash(_name_)
 
 def serve_layout():
-read = pd.read_excel('wikipagin.xlsx')
+  read = pd.read_excel('wikipagin.xlsx')
 
-return read.Div([html.H1(read['Titulo']),
+  return read.Div([html.H1(read['Titulo']),
                
-html.Div(read['Tema']),
+                        html.Div(read['Tema']),
               
-html.Div(read['Ayuda'])])
+                        html.Div(read['Ayuda'])])
 
 
 app.layout =serve_layout
 
 if_name== 'main_':
 
-app.run_server(host='0,0,0,0',port=80)
+  #Objeto a todas las interfaces de red en puerto 80
+   app.run_server(host='0,0,0,0',port=80)
