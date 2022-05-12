8e308b2ee9a2539f18da6cb07c27adf0358cc473
vero2002257257 <105020892+vero2002257257@users.noreply.github.com>
2022-05-12 17:04:11 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
 #Este código lo puedes utilizar si deseas
 #1.Llamar las librerias
-from flask import Flask
+from dash import Dash, html, dcc
+import plotly.graph objs as go
+import pandas as pd
 
-app = Flask(__name__)
+app = Dash(_name_)
+
+def serve_layout():
+read = pd.read_excel('wikipagin.xlsx')
+
+return read.Div([html.H1(read['Titulo']),
+               
+html.Div(read['Tema']),
+
+html.Div(read['Info']),
+              
+html.Div(read['Ayuda'])])])
 
 
+app.layout =serve_layout
 
-def hello():
-    return 'Bienvenido!'
+if_name== 'main_':
+
+app.run_server(host='0,0,0,0',port=80)
